9273e199e7cd583b6660a5a2449e5f2247404983
Zander Martineau <zander@martineau.tv>
2014-07-27 22:02:40 +0100

    improve stars by removing the default star char and replacing it with another using CSS

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -8,6 +8,11 @@ chrome.extension.sendMessage({}, function(response) {
 			 */
 			var searchInput = document.getElementById('search_query_field');
 			searchInput.setAttribute('placeholder', 'Search Pinboard');
+
+			var star = document.querySelectorAll('.star span');
+			for (var i = star.length - 1; i >= 0; i--) {
+				star[i].innerHTML = '';
+			}
 		}
 	}, 10);
 });
